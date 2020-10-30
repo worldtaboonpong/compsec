f0f8286a667ca78a17afd9d4a704608f60078608
World <catsfinetoo@gmail.com>
2020-10-30 19:44:53 +0700

    add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Switch, Route, BrowserRouter, Link } from "react-router-dom";
+import Home from "./views/Home.js";
+import Register from "./views/Register.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="collapse navbar-collapse" id="navbarSupportContent">
+            <ul className="navbar-nav mr-auto container">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item ">
+                <Link className="nav-link" to="/register">
+                  Register
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </div>
+      <div className="container">Footer</div>
     </div>
   );
 }
